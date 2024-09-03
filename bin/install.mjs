9d4d72e257a087d8d62ec6836ff36d9d8fd7d510
João Pedro Buzato <joaobuzato@gmail.com>
2024-09-02 21:33:09 -0300

    readding spinners

--- bin/install.mjs
+++ bin/install.mjs
@@ -1,16 +1,41 @@
 #!/usr/bin/env node
+
 import { promisify } from "util";
 import cp from "child_process";
 import path from "path";
 import fs from "fs";
+import readline from "readline";
+import cliSpinners from "cli-spinners";
 
-// convert libs to promises
+// Convert libraries to promises
 const exec = promisify(cp.exec);
 const rm = promisify(fs.rm);
 
+// Spinner setup
+const spinner = cliSpinners.dots;
+const spinnerInterval = 100;
+let spinnerIntervalId;
+
+// Function to start the spinner
+function startSpinner() {
+  let frame = 0;
+  spinnerIntervalId = setInterval(() => {
+    readline.cursorTo(process.stdout, 0);
+    process.stdout.write(`${spinner.frames[frame]} ${spinner.text}`);
+    frame = (frame + 1) % spinner.frames.length;
+  }, spinnerInterval);
+}
+
+// Function to stop the spinner
+function stopSpinner() {
+  clearInterval(spinnerIntervalId);
+  readline.cursorTo(process.stdout, 0);
+  process.stdout.write("Done!                   \n");
+}
+
 if (process.argv.length < 3) {
   console.log("You have to provide a name to your app.");
-  console.log("For example :");
+  console.log("For example:");
   console.log("    npx create-express-api my-app");
   process.exit(1);
 }
@@ -19,11 +44,11 @@ const projectName = process.argv[2];
 const currentPath = process.cwd();
 const projectPath = path.join(currentPath, projectName);
 
-const git_repo = "https://github.com/joaobuzato/express-api-boilerplate.git";
+const gitRepo = "https://github.com/joaobuzato/express-api-boilerplate.git";
 
 if (fs.existsSync(projectPath)) {
   console.log(
-    `The file ${projectName} already exist in the current directory, please give it another name.`
+    `The file ${projectName} already exists in the current directory. Please choose another name.`
   );
   process.exit(1);
 } else {
@@ -31,7 +56,8 @@ if (fs.existsSync(projectPath)) {
 }
 
 try {
-  await exec(`git clone --depth 1 ${git_repo} ${projectPath} --quiet`);
+  startSpinner();
+  await exec(`git clone --depth 1 ${gitRepo} ${projectPath} --quiet`);
 
   const rmGit = rm(path.join(projectPath, ".git"), {
     recursive: true,
@@ -42,17 +68,21 @@ try {
     recursive: true,
     force: true,
   });
+
   await Promise.all([rmGit, rmBin]);
 
   process.chdir(projectPath);
 
   await exec("npm install");
 
+  stopSpinner();
+
   console.log("The installation is done!");
   console.log("You can now run your app with:");
   console.log(`    cd ${projectName}`);
-  console.log(`    npm run start`);
+  console.log("    npm run start");
 } catch (error) {
+  stopSpinner();
   fs.rmSync(projectPath, { recursive: true, force: true });
-  console.log(error);
+  console.error("An error occurred:", error);
 }
